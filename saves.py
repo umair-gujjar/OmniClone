4e96ebb28d0acfa85569543f92a93b915992753a
Vincent Mader <vincent.mader@icloud.com>
2017-07-15 17:56:32 +0200

    Update saves.py

--- saves.py
+++ saves.py
@@ -11,7 +11,35 @@ tags        = []
 def list_to_file(filename):
     with open(filename, 'r+') as fp:
         fp.truncate()
-        if filename == 'projects.txt':
+
+        if filename == 'actions.txt':
+            all_data = []
+            for a in actions:
+                action_data = []
+                for d in [a.comment, a.completed, a.dateAdded, a.dateChanged, a.deferUntil, a.displayIndex, a.due, a.estTime, a.flagged, a.project, a.status, a.tags, a.title]:
+                    action_data.append(d)
+                all_data.append(action_data)
+            json.dump(all_data, fp)
+            
+        elif filename == 'events.txt':
+            all_data = []
+            for e in events:
+                event_data = []
+                for d in [e.comment, e.dateStart, e.dateEnd, e.project, e.tags, e.title]:
+                    event_data.append(d)
+                all_data.append(event_data)
+            json.dump(all_data, fp)
+        
+        elif filename == 'inbox.txt':
+            all_data = []
+            for i in inbox:
+                inbox_data = []
+                for d in [i.comment, i.completed, i.dateAdded, i.dateChanged, i.deferUntil, i.displayIndex, i.due, i.estTime, i.flagged, i.project, i.status, i.tags, i.title]:
+                    inbox_data.append(d)
+                all_data.append(inbox_data)
+            json.dump(all_data, fp)
+            
+        elif filename == 'projects.txt':
             all_data = []
             for p in projects:
                 project_data = []
@@ -19,12 +47,18 @@ def list_to_file(filename):
                     project_data.append(d)
                 all_data.append(project_data)
             json.dump(all_data, fp)
+            
+        elif filename == 'tags.txt':
+            json.dump(tags)
+            
         else:
-            None
+            print('ERROR in saves.py: Wrong \'filename\' passed to list_to_file()')
+
 
 
 def file_to_list(filename):
     with open(filename, 'r') as fp:
+        if filename == ''
         a = json.load(fp)
     print(a)
 
