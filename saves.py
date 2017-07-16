50511d2580ffb63e65a70f238b78503bd9953687
Vincent Mader <vincent.mader@icloud.com>
2017-07-16 02:56:00 +0200

    Update saves.py

--- saves.py
+++ saves.py
@@ -7,68 +7,212 @@ events      = []
 inbox       = []
 tags        = []
 
+sorting_key_a = 'title'
+sorting_key_i = 'title'
+sorting_key_p = 'title'
+
+
+def sort_actions(sorting_key):
+    global sorting_key_a
+    sorting_key_a = sorting_key
+    
+    if sorting_key == 'comment':
+        pass
+    elif sorting_key == 'completed':
+        pass
+    elif sorting_key == 'dateAdded':
+        pass
+    elif sorting_key == 'dateChanged':
+        pass
+    elif sorting_key == 'deferUntil':
+        pass
+    elif sorting_key == 'displayIndex':
+        pass
+    elif sorting_key == 'due':
+        pass
+    elif sorting_key == 'estTime':
+        pass
+    elif sorting_key == 'flagged':
+        pass
+    elif sorting_key == 'project':
+        pass
+    elif sorting_key == 'status':
+        pass
+    elif sorting_key == 'tags':
+        pass
+    elif sorting_key == 'title':
+        pass
+    
+
+def sort_inbox(sorting_key):
+    global sorting_key_i
+    sorting_key_i = sorting_key
+    
+    if sorting_key == 'comment':
+        pass
+    elif sorting_key == 'completed':
+        pass
+    elif sorting_key == 'dateAdded':
+        pass
+    elif sorting_key == 'dateChanged':
+        pass
+    elif sorting_key == 'deferUntil':
+        pass
+    elif sorting_key == 'displayIndex':
+        pass
+    elif sorting_key == 'due':
+        pass
+    elif sorting_key == 'estTime':
+        pass
+    elif sorting_key == 'flagged':
+        pass
+    elif sorting_key == 'project':
+        pass
+    elif sorting_key == 'status':
+        pass
+    elif sorting_key == 'tags':
+        pass
+    elif sorting_key == 'title':
+        pass
+    
+
+def sort_projects(sorting_key):
+    global sorting_key_p
+    sorting_key_p = sorting_key
+    
+    if sorting_key == 'comment':
+        pass
+    elif sorting_key == 'completed':
+        pass
+    elif sorting_key == 'dateAdded':
+        pass
+    elif sorting_key == 'dateChanged':
+        pass
+    elif sorting_key == 'deferUntil':
+        pass
+    elif sorting_key == 'displayIndex':
+        pass
+    elif sorting_key == 'due':
+        pass
+    elif sorting_key == 'estTime':
+        pass
+    elif sorting_key == 'flagged':
+        pass
+    elif sorting_key == 'status':
+        pass
+    elif sorting_key == 'tags':
+        pass
+    elif sorting_key == 'title':
+        pass
+
 
 def list_to_file(filename):
+
     with open(filename, 'r+') as fp:
-        if fp.readlines() == []:
-            # file is empty, json module cannot load it -> leave list as it is
-            pass
+
+        fp.truncate()
+
+        if filename == 'actions.txt':
+            all_data = []
+            for a in actions:
+                action_data = []
+                for d in [a.comment,
+                          a.completed,
+                          a.dateAdded,
+                          a.dateChanged,
+                          a.deferUntil,
+                          a.displayIndex,
+                          a.due,
+                          a.estTime,
+                          a.flagged,
+                          a.project,
+                          a.status,
+                          a.tags,
+                          a.title
+                          ]:
+                    action_data.append(d)
+                all_data.append(action_data)
+            json.dump(all_data, fp)
+
+        elif filename == 'events.txt':
+            all_data = []
+            for e in events:
+                event_data = []
+                for d in [e.comment,
+                          e.dateStart,
+                          e.dateEnd,
+                          e.project,
+                          e.tags,
+                          e.title
+                          ]:
+                    event_data.append(d)
+                all_data.append(event_data)
+            json.dump(all_data, fp)
+
+        elif filename == 'inbox.txt':
+            all_data = []
+            for i in inbox:
+                inbox_data = []
+                for d in [i.comment,
+                          i.completed,
+                          i.dateAdded,
+                          i.dateChanged,
+                          i.deferUntil,
+                          i.displayIndex,
+                          i.due,
+                          i.estTime,
+                          i.flagged,
+                          i.project,
+                          i.status,
+                          i.tags,
+                          i.title]:
+                    inbox_data.append(d)
+                all_data.append(inbox_data)
+            json.dump(all_data, fp)
+
+        elif filename == 'projects.txt':
+            all_data = []
+            for p in projects:
+                project_data = []
+                for d in [p.comment,
+                          p.completed,
+                          p.dateAdded,
+                          p.dateChanged,
+                          p.deferUntil,
+                          p.displayIndex,
+                          p.due,
+                          p.estTime,
+                          p.flagged,
+                          p.status,
+                          p.tags,
+                          p.title]:
+                    project_data.append(d)
+                all_data.append(project_data)
+            json.dump(all_data, fp)
+
+        elif filename == 'tags.txt':
+            json.dump(tags, fp)
 
         else:
-            fp.truncate()
-
-            if filename == 'actions.txt':
-                all_data = []
-                for a in actions:
-                    action_data = []
-                    for d in [a.comment, a.completed, a.dateAdded, a.dateChanged, a.deferUntil, a.displayIndex, a.due, a.estTime, a.flagged, a.project, a.status, a.tags, a.title]:
-                        action_data.append(d)
-                    all_data.append(action_data)
-                json.dump(all_data, fp)
-
-            elif filename == 'events.txt':
-                all_data = []
-                for e in events:
-                    event_data = []
-                    for d in [e.comment, e.dateStart, e.dateEnd, e.project, e.tags, e.title]:
-                        event_data.append(d)
-                    all_data.append(event_data)
-                json.dump(all_data, fp)
-
-            elif filename == 'inbox.txt':
-                all_data = []
-                for i in inbox:
-                    inbox_data = []
-                    for d in [i.comment, i.completed, i.dateAdded, i.dateChanged, i.deferUntil, i.displayIndex, i.due, i.estTime, i.flagged, i.project, i.status, i.tags, i.title]:
-                        inbox_data.append(d)
-                    all_data.append(inbox_data)
-                json.dump(all_data, fp)
-
-            elif filename == 'projects.txt':
-                all_data = []
-                for p in projects:
-                    project_data = []
-                    for d in [p.comment, p.completed, p.dateAdded, p.dateChanged, p.deferUntil, p.displayIndex, p.due, p.estTime, p.flagged, p.status, p.tags, p.title]:
-                        project_data.append(d)
-                    all_data.append(project_data)
-                json.dump(all_data, fp)
-
-            elif filename == 'tags.txt':
-                json.dump(tags, fp)
-
-            else:
-                print('ERROR in saves.py: Wrong \'filename\' passed to list_to_file() method')
-
-
+            print('ERROR in saves.py: Wrong \'filename\' passed to list_to_file() method')
 
 
 def file_to_list(filename):
 
-    with open(filename, 'r') as fp:
+    fp = open(filename, 'r')
+    json_string = fp.read()
+
+    if len(json_string) == 0:
+
+        # file is empty, json module cannot load it -> leave list as it is
+        pass
+
+
+    elif len(json_string) != 0:
 
         if filename == 'actions.txt':
 
-            all_data = json.load(fp)
+            all_data = json.loads(json_string)
             global actions
             actions = []
 
@@ -95,7 +239,7 @@ def file_to_list(filename):
 
         elif filename == 'events.txt':
 
-            all_data = json.load(fp)
+            all_data = json.loads(json_string)
             global events
             events = []
 
@@ -114,7 +258,7 @@ def file_to_list(filename):
 
         elif filename == 'inbox.txt':
 
-            all_data = json.load(fp)
+            all_data = json.loads(json_string)
             global inbox
             inbox = []
 
@@ -140,7 +284,7 @@ def file_to_list(filename):
 
         elif filename == 'projects.txt':
 
-            all_data = json.load(fp)
+            all_data = json.loads(json_string)
             global projects
             projects = []
 
@@ -163,12 +307,19 @@ def file_to_list(filename):
 
                 projects.append(p)
 
+            print(projects)
+
 
         elif filename == 'tags.txt':
 
             global tags
-            tags = json.load(fp)
+            tags = json.loads(json_string)
 
 
         else:
             print('ERROR in saves.py: Wrong \'filename\' passed to file_to_list() method')
+
+    else:
+        print('This shouldn\'t happen. (else-statement in saves.file_to_ist())')
+
+        fp.close()
